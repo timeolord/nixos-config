fca860f4db37a01e3a8e3e4b5282e69c86fbcb14
melk <timeolord6677@gmail.com>
2025-10-09 02:50:18 -0400

    test

--- configuration.nix
+++ configuration.nix
@@ -99,6 +99,7 @@
   git
   emacs
   bitwarden-desktop
+  youtube-music
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
