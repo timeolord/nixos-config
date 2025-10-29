3c5b86c08b407d00c73dfeba71fccf30887654a6
melk <>
2025-10-29 14:31:27 -0400

    updated configs

--- configuration.nix
+++ configuration.nix
@@ -86,6 +86,29 @@
     #media-session.enable = true;
   };
 
+  # Bluetooth stuff
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        # Shows battery charge of connected devices on supported
+        # Bluetooth adapters. Defaults to 'false'.
+        Experimental = true;
+        # When enabled other devices can connect faster to us, however
+        # the tradeoff is increased power consumption. Defaults to
+        # 'false'.
+        FastConnectable = true;
+      };
+      Policy = {
+        # Enable all controllers when they are found. This includes
+        # adapters present on start as well as adapters that are plugged
+        # in later on. Defaults to 'true'.
+        AutoEnable = true;
+      };
+    };
+  };
+  services.blueman.enable = true;
   # Enable touchpad support (enabled default in most desktopManager).
   # services.libinput.enable = true;
 
@@ -120,8 +143,6 @@
     # nix-index
     babelfish
     # Hyprland
-    blueman
-    bluez
     hyprpolkitagent
     xorg.xrandr
     xdg-desktop-portal-gtk
