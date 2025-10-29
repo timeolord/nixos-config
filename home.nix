c792f073fadce7e55b21b71557c5257357393d69
melk <>
2025-10-29 16:26:14 -0400

    updated configs

--- home.nix
+++ home.nix
@@ -107,7 +107,7 @@
         };
         clock = {
           interval = 1;
-          format = "{:%A} :%I:%M}";
+          format = "{:%A} {:%I:%M}";
           #tooltip-format = "{:%Y-%m-%d}";
         };
         pulseaudio = {
