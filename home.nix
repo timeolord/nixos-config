7fd4043be1740944f046dbc467b1c897705a4e39
melk <>
2025-10-29 00:00:46 -0400

    updated configs

--- home.nix
+++ home.nix
@@ -263,8 +263,8 @@
         prompt = "f4d9e1ff";
         placeholder="838ba7ff";
         input="f4d9e1ff";
-        match="ea999cff";
-        selection="626880ff";
+        match="ffc40080";
+        selection="8c00ffff";
         selection-text="ff3f7fff";
         selection-match="ffce00ff";
         counter="8c00ffff";
