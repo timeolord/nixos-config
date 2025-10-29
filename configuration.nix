c4f3890d48bc0d290177af85605c1fba2b0c316f
melk <>
2025-10-29 14:49:11 -0400

    updated configs

--- configuration.nix
+++ configuration.nix
@@ -199,6 +199,10 @@
     options = "--delete-older-than 7d";
   };
 
+  system.autoUpgrade = {
+    enable = true;
+  };
+
     # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
