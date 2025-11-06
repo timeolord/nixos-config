a42fc524e470764079fdc15c41258deca2d77884
timeolord <timeolord6677@gmail.com>
2025-11-06 09:28:18 -0500

    updated configs

--- rebuild.sh
+++ rebuild.sh
@@ -4,8 +4,12 @@
 
 cd /etc/nixos
 git pull
-sudo -- bash -c "nixos-rebuild switch --flake '/etc/nixos#${USER}'"
-# Update Git Repo
-git add -u
-git commit -m "updated configs"
-git push
+sudo nixos-rebuild switch --flake /etc/nixos#${USER}
+if [ $? -eq 0 ]; then
+    # Update Git Repo
+    git add -u
+    git commit -m "updated configs"
+    git push
+else
+    sudo nixos-rebuild switch --flake /etc/nixos#${USER} --show-trace
+fi
