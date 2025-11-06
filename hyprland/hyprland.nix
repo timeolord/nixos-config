06e46b026233df45094bc1dd8bd8d59f5337594c
timeolord <timeolord6677@gmail.com>
2025-11-06 12:13:01 -0500

    updated configs

--- hyprland/hyprland.nix
+++ hyprland/hyprland.nix
@@ -21,10 +21,10 @@
       # bigclock_12hr = "true";
 
       animation = "gameoflife";
-      gameoflife_entropy_interval = 10;
-      gameoflife_fg = "0x0000FF00";
-      gameoflife_frame_delay = 6;
-      gameoflife_initial_density = "0.4";
+      # gameoflife_entropy_interval = 10;
+      # gameoflife_fg = "0x0000FF00";
+      # gameoflife_frame_delay = 6;
+      # gameoflife_initial_density = "0.4";
 
       text_in_center = true;
     };
