63ae098c53e20e29223a8f54c76ed4771b5b61e0
timeolord <timeolord6677@gmail.com>
2025-11-06 11:57:29 -0500

    updated configs

--- hyprland/hyprland.nix
+++ hyprland/hyprland.nix
@@ -14,7 +14,21 @@
   ];
   
   services.xserver.enable = false;
-  services.displayManager.ly.enable = true;
+  services.displayManager.ly = {
+    enable = true;
+    settings = {
+      bigclock = "en";
+      bigclock_12hr = "true";
+
+      animation = "gameoflife";
+      gameoflife_entropy_interval = 10;
+      gameoflife_fg = "0x0000FF00";
+      gameoflife_frame_delay = 6;
+      gameoflife_initial_density = "0.4";
+
+      text_in_center = true;
+    };
+  };
   
   programs.hyprland = {
     enable = true;
