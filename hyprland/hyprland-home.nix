39c6e66e88dd7e86da3fafd9fa8490dca9d003f3
timeolord <timeolord6677@gmail.com>
2025-11-06 08:57:49 -0500

    updated configs

---
+++ hyprland/hyprland-home.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, userName, ... }:
+let
+  hyprland_base_config = builtins.readFile ./hyprland.conf;
+  additional_config = builtins.readFile (./. + builtins.toPath "${userName.conf}");
+  hyprland_config = hyprland_base_config + additional_config;
+in
+{
+  imports = [
+    ./kitty.nix
+    ./waybar.nix
+    ./dunst.nix
+    ./hyprpaper.nix
+    ./fuzzel.nix
+  ];
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    package = null;
+    portalPackage = null;
+    systemd.variables = [ "--all" ];
+    extraConfig = hyprland_config;
+  };
+
+  home.packages = with pkgs; [
+    hyprpolkitagent
+    xorg.xrandr
+    xdg-desktop-portal-gtk
+    pavucontrol
+    pipewire
+    wireplumber
+  ];
+}
