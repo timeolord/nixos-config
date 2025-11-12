07b089af67561ea31658d4244fad1af51bd14f63
timeolord <timeolord6677@gmail.com>
2025-11-12 09:59:14 -0500

    updated configs

--- config.nix
+++ config.nix
@@ -189,9 +189,6 @@
     auto-optimise-store = true;
     # substitute = false; # builds all packages from source (Too slow)
   };
-  system.autoUpgrade = {
-    enable = true;
-  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
