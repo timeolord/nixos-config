11a10a36c5614ded35d2b84f796df881f0a7fac0
timeolord <timeolord6677@gmail.com>
2025-11-12 09:27:40 -0500

    updated configs

--- programs/fish/fish.nix
+++ programs/fish/fish.nix
@@ -31,7 +31,7 @@
     };
     shellInit = ''
       set PATH /etc/nixos /home/${userName}/.local/bin $PATH
-      set -gx EDITOR "emacs 2> /dev/null"
+      set -gx EDITOR emacs
       function fish_greeting
          fastfetch
          echo "God Says..."
--- programs/fish/functions.fish
+++ programs/fish/functions.fish
@@ -2,3 +2,7 @@ function ghcl -a username repo_name
     set repo_link git@github.com:{$username}/{$repo_name}.git
     command git clone $repo_link
 end
+function gaup -a commit_message
+    command git add -a
+    command commit -m $commit_message
+end
