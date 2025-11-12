c1d4d0933c2c3e42e5efb25e29de5f0bdbb7e3f0
timeolord <timeolord6677@gmail.com>
2025-11-12 09:33:34 -0500

    updated configs

--- programs/fish/functions.fish
+++ programs/fish/functions.fish
@@ -2,7 +2,8 @@ function ghcl -a username repo_name
     set repo_link git@github.com:{$username}/{$repo_name}.git
     command git clone $repo_link
 end
-function gaup -a commit_message
+function gacp -a commit_message
     command git add -A
     command git commit -m $commit_message
+    command git push
 end
