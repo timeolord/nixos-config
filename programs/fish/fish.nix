4bb3864fa1179b10bc2ad2e8bdec01db6f2a2995
timeolord <timeolord6677@gmail.com>
2025-11-15 23:55:02 -0500

    updated configs

--- programs/fish/fish.nix
+++ programs/fish/fish.nix
@@ -36,6 +36,7 @@
       set PYTHONSTARTUP /etc/nixos/python/startup.py
       set -gx EDITOR emacs
       function fish_greeting
+         rm -ri /home/${userName}/Downloads 
          fastfetch
          echo "God Says..."
          godsays
