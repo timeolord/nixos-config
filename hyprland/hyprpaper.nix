298652ca308cd34f56600468a1c80acc70c2c311
timeolord <timeolord6677@gmail.com>
2026-02-02 13:54:01 -0500

    updated configs

--- hyprland/hyprpaper.nix
+++ hyprland/hyprpaper.nix
@@ -5,12 +5,14 @@
   services.hyprpaper = {
     enable = true;
     settings = {
-      preload = [
-        "~/.wallpapers/ivan_the_terrible.jpg"
-      ];
-      wallpaper = [
-        ", ~/.wallpapers/ivan_the_terrible.jpg"
-      ];
+      # preload = [
+      #   "~/.wallpapers/ivan_the_terrible.jpg"
+      # ];
+      wallpaper = {
+        monitor = " ";
+        path = ", ~/.wallpapers/ivan_the_terrible.jpg";
+        fit_mode = "cover";
+      };
     };
   };
 }
