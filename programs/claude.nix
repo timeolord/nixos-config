f753e6b73d509430955a5029751255dd9cdc5003
timeolord <timeolord6677@gmail.com>
2026-03-31 16:20:46 -0400

    updated configs

--- programs/claude.nix
+++ programs/claude.nix
@@ -6,11 +6,11 @@
 }:
 {
   home.file.".claude/CLAUDE.md".text = ''
-    I use NixOS, so for each for all dependencies make sure there is a flake.nix, along with a .envrc with "use flake". All comments should be lowercase written informally, without the use of any dashes. I prefer concise one liners and functional programming patterns. I prefer to use the languages Rust, Zig, and Haskell. I want functions to be named in snakecase. In Zig I do not want any default values to be used. If a default value is needed, a constructor should be used instead.
+    I use NixOS, so for all dependencies make sure there is a flake.nix, along with a .envrc with "use flake". All comments should be lowercase written informally, without the use of any dashes. I prefer concise one liners and functional programming patterns. I prefer to use the languages Rust, Zig, and Haskell. I want functions to be named in snakecase. In Zig I do not want any default values to be used. If a default value is needed, a constructor should be used instead.
 '';
   home.file.".claude/settings.json".text = ''
 {
-  "effortLevel": "medium",
+  "effortLevel": "medium"
   "env": {
     "CLAUDE_AUTOCOMPACT_PCT_OVERRIDE": "50",
     "CLAUDE_CODE_SUBAGENT_MODEL": "haiku",
@@ -27,5 +27,6 @@
 
   home.packages = with pkgs; [
     claude-code
+    codex
   ];
 }
