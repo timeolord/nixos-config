6863fea3089d3c181b1403d4e1a8b8e7441da267
timeolord <timeolord6677@gmail.com>
2026-05-26 07:10:29 -0400

    updated configs

--- melkmobile.nix
+++ melkmobile.nix
@@ -5,4 +5,9 @@
 }:
 {
   imports = [./nvidia.nix];
+  services.logind.settings.Login = {
+    HandleLidSwitch = "ignore";
+    HandleLidSwitchExternalPower = "ignore";
+    HandleLidSwitchDocked = "ignore";
+  };
 }
