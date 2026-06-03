0100d4e459d3d6b287bb80a2b31b577de93c8b5f
timeolord <timeolord6677@gmail.com>
2026-06-03 16:23:32 -0400

    updated configs

--- programs/emacs.el
+++ programs/emacs.el
@@ -424,7 +424,13 @@
 
 (use-package org-roam
   :ensure t
-  :defer t)
+  :defer t
+  :custom
+  (org-roam-directory "~/notes")
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert))
+  )
 
 (use-package wgsl-mode
   :ensure t
