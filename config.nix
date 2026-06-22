0709dcf5ff4841bfc7c15e18c5a4dcaf86f1c2de
timeolord <timeolord6677@gmail.com>
2026-06-22 17:37:04 -0400

    updated configs

--- config.nix
+++ config.nix
@@ -64,8 +64,10 @@
     ];
     substituters = [
       "https://nix-community.cachix.org"
+      "https://cache.nixos-cuda.org"
     ];
     trusted-public-keys = [
+      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
     auto-optimise-store = true;
