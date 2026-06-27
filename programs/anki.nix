94e819fd2b8ff2c660b083b1d0e9f5ba7dfeec01
timeolord <timeolord6677@gmail.com>
2026-06-27 16:44:49 -0400

    updated configs

--- programs/anki.nix
+++ programs/anki.nix
@@ -4,8 +4,18 @@
   userName,
   ...
 }:
+let
+  pythonWithSpacy = pkgs.python313.withPackages (ps: [ ps.spacy ]);
+  ankiWithSpacy = pkgs.symlinkJoin {
+    name = "anki-with-spacy";
+    paths = [ pkgs.anki ];
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/anki \
+        --prefix PYTHONPATH : "${pythonWithSpacy}/${pkgs.python313.sitePackages}"
+    '';
+  };
+in
 {
-  home.packages = with pkgs; [
-    anki
-  ];
+  home.packages = [ ankiWithSpacy ];
 }
